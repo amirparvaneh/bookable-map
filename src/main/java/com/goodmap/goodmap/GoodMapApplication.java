f9317726d8cd44095a269d2304b70f33ad342d2d
amirparvaneh <amirparvaneh138@gmail.com>
2022-08-31 12:42:00 +0430

    a

---
+++ src/main/java/com/goodmap/goodmap/GoodMapApplication.java
@@ -0,0 +1,13 @@
+package com.goodmap.goodmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodMapApplication.class, args);
+    }
+
+}
